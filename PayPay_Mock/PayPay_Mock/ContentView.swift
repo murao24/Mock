dcb155431252b60f60e5b9ded24494cffff6ec05
Murao <yoshinobu612@icloud.com>
2021-06-15 10:31:50 +0900

    PayPay_Mock_Start

---
+++ PayPay_Mock/PayPay_Mock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PayPay_Mock
+//
+//  Created by 村尾慶伸 on 2021/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PayPay_Mock/PayPay_Mock/PayPay_MockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPay_MockApp.swift
+//  PayPay_Mock
+//
+//  Created by 村尾慶伸 on 2021/06/15.
+//
+
+import SwiftUI
+
+@main
+struct PayPay_MockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
