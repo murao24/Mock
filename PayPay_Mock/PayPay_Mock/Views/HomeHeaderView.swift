48036263e02820502a0e9713b0c4ee34b507f3b9
Murao <yoshinobu612@icloud.com>
2021-06-16 10:29:46 +0900

    arrange HomeHeaderView

--- PayPay_Mock/PayPay_Mock/Views/HomeHeaderView.swift
+++ PayPay_Mock/PayPay_Mock/Views/HomeHeaderView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct HomeHeaderView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack(alignment: .topTrailing) {
+            
+            Color.red.opacity(0.7).ignoresSafeArea()
+            
+            HStack {
+                
+                Spacer()
+                
+                CardView()
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    .shadow(radius: 40)
+                    .padding(.top, 20)
+                
+                Spacer()
+            }
+            
+            Image(systemName: "bell")
+                .padding(6)
+                .font(.title)
+                .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(20)
+                .padding(.trailing, 20)
+        }
     }
 }
 
 struct HomeHeaderView_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         HomeHeaderView()
     }
 }
+
